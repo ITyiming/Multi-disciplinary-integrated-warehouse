7ee240ac0fe890c7823a1e4c1df88894ef81a29a
ITyiming <2844114018@qq.com>
2024-03-09 12:53:22 +0800

    submit new energy

---
+++ 新能源科学与工程/机械制图PPS课件/xt/xt6web/5.1/xmsg_adsk_ptw_all.js
@@ -0,0 +1,38 @@
+﻿/* 
+	Copyright 1988-2000 by Autodesk, Inc.
+
+	Permission to use, copy, modify, and distribute this software
+	for any purpose and without fee is hereby granted, provided
+	that the above copyright notice appears in all copies and
+	that both that copyright notice and the limited warranty and
+	restricted rights notice below appear in all supporting
+	documentation.
+
+	AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+	AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+	MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+	DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+	UNINTERRUPTED OR ERROR FREE.
+
+	Use, duplication, or disclosure by the U.S. Government is subject to
+	restrictions set forth in FAR 52.227-19 (Commercial Computer
+	Software - Restricted Rights) and DFAR 252.227-7013(c)(1)(ii) 
+	(Rights in Technical Data and Computer Software), as applicable.
+
+    Global strings
+*/
+
+// LOCALIZATION_BEGIN
+var xmsg_adsk_ptw_all_summaryTitle = "标题: ";
+var xmsg_adsk_ptw_all_summarySubject = "主题: ";
+var xmsg_adsk_ptw_all_summaryAuthor = "作者: ";
+var xmsg_adsk_ptw_all_summaryKeywords = "关键字: ";
+var xmsg_adsk_ptw_all_summaryComments = "注释: ";
+var xmsg_adsk_ptw_all_summaryHyperlinkBase = "超级链接基地址: ";
+var xmsg_adsk_ptw_all_validate_viewer = "本网页包含一些 Design Web Format(TM)  (DWF) 图像，\n这些图像只能在安装了 Autodesk(R) Express Viewer\n(版本 3.0 或更高版本) 的系统上显示。\n此查看器是免费的，您可以前往 Autodesk(R) Express Viewer 网站，以便安装该查看器。\n是否要立即前往 Autodesk(R) Express Viewer 网站？";
+var xmsg_adsk_ptw_all_validate_viewer_url = "http://www.autodesk.com/viewers"; // Autodesk?Express Viewer Home Page
+var xmsg_adsk_ptw_all_idrop_url = "http://idrop.autodesk.com/files/idrop.cab";        // Code base for iDrop OCX
+var xmsg_adsk_ptw_all_contents_text = "内容";
+// LOCALIZATION_END
+
+
